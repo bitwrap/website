21f57d738bd626729d0df5a67b81c333589e2a27
Ayush Sehrawat <itsayushsehrawat@gmail.com>
2023-03-15 22:49:47 +0530

    fixes and bugs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-import prefetch from '@astrojs/prefetch';
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
 import sitemap from "@astrojs/sitemap";
@@ -7,5 +6,5 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bitwrap.neobrains.me',
-  integrations: [tailwind(), prefetch(), compress(), sitemap()]
+  integrations: [tailwind(), compress(), sitemap()]
 });
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,20 @@
-//
+let isExpanded = false;
+const toggle = document.getElementById('toggle');
+const menu = document.getElementById('menu');
+
+toggle.addEventListener('click', () => {
+    if (isExpanded) {
+        // open with transition
+        menu.style.display = 'none';
+        isExpanded = false;
+    } else {
+        menu.style.display = 'block';
+        isExpanded = true;
+    }
+});
+
+// get width on load of page
+const width = window.innerWidth;
+if (width < 768) {
+    menu.style.display = 'none';
+}
