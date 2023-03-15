229928448d9b17a1f362f556a868908231026e5d
Ayush Sehrawat <itsayushsehrawat@gmail.com>
2023-03-15 22:12:24 +0530

    Added content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config';
-import cloudflare from "@astrojs/cloudflare";
 import prefetch from '@astrojs/prefetch';
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: cloudflare(),
   site: 'https://bitwrap.neobrains.me',
-  integrations: [tailwind(),prefetch(),compress()]
+  integrations: [tailwind(), prefetch(), compress(), sitemap()]
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,11 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			colors: {
+				"div-bg": "#ffffff0f",
+				"hover-div-bg": "#ffffff1f",
+				"menu": "#ffffff3f",
+			  },
 			fontFamily :{
 				"exo-2": ["'Exo 2'", "sans-serif"],
 				lato: ["Lato", "sans-serif"],
