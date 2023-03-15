a64731da7a40bc03cdbf6609528bd0602cce8509
Ayush Sehrawat <itsayushsehrawat@gmail.com>
2023-03-15 22:54:05 +0530

    more fixed

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,20 +1,22 @@
 let isExpanded = false;
-const toggle = document.getElementById('toggle');
-const menu = document.getElementById('menu');
+const toggle = document.getElementById("toggle");
+const menu = document.getElementById("menu");
 
-toggle.addEventListener('click', () => {
-    if (isExpanded) {
-        // open with transition
-        menu.style.display = 'none';
-        isExpanded = false;
-    } else {
-        menu.style.display = 'block';
-        isExpanded = true;
-    }
+toggle.addEventListener("click", () => {
+  if (isExpanded) {
+    // open with transition
+    menu.style.display = "none";
+    isExpanded = false;
+  } else {
+    menu.style.display = "block";
+    isExpanded = true;
+  }
 });
 
-// get width on load of page
-const width = window.innerWidth;
-if (width < 768) {
-    menu.style.display = 'none';
-}
+window.addEventListener("resize", () => {
+    if (window.innerWidth > 1024) {
+      isExpanded = false;
+      menu.style.display = "none";
+    }
+  });
+  
