eaa3cca893ab266c1434683729d507b5e2f4a4d4
Ayush Sehrawat <itsayushsehrawat@gmail.com>
2023-03-16 19:52:55 +0530

    Moved to bitwrap.cloud

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://bitwrap.neobrains.me',
+  site: 'https://bitwrap.cloud',
   integrations: [tailwind(), compress(), sitemap()]
 });
